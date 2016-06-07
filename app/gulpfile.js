2fc0e37d0e0743765d31304278aae033753a13cf
fpiaggio <fipiaggiodev@gmail.com>
2016-06-07 01:02:39 -0300

    Estructura base definida Carpetas app y api para separar backend y frontend

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -3,20 +3,20 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
 gulp.task('styles', function() {
-    gulp.src('./app/scss/main.scss')
+    gulp.src('./scss/main.scss')
         .pipe(sass({
             outputStyle: 'compressed'
         }).on('error', sass.logError))
-        .pipe(gulp.dest('./app/css'))
+        .pipe(gulp.dest('./css'))
         .pipe(browserSync.reload({
             stream: true
         }));
-    gulp.src('./app/scss/materialize.scss')
+    gulp.src('./scss/materialize.scss')
         .pipe(sass({
             outputStyle: 'compressed'
         }))
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./app/css'))
+        .pipe(gulp.dest('./css'))
         .pipe(browserSync.reload({
             stream: true
         }));
@@ -26,11 +26,11 @@ gulp.task('serve', function() {
 
     browserSync.init({
         server: {
-            baseDir: './app/'
+            baseDir: './'
         }
     })
-    gulp.watch('./app/**/*.scss', ['styles']);
-    gulp.watch('./app/**/*.html').on('change', browserSync.reload);
+    gulp.watch('./**/*.scss', ['styles']);
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['styles', 'serve']);
